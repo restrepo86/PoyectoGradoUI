f945d44937dfcf244fd592fce3f4c5814141d41a
Juan Fernando Restrepo <JuanRestrepo>
2019-04-10 23:07:15 -0500

    Se agrega correccion a guardado de data de consulta de planes de estudio en ssesionStorage

--- src/views/components/ListComponent/index.js
+++ src/views/components/ListComponent/index.js
@@ -17,7 +17,7 @@ class ListComponent extends React.Component {
 
   componentDidMount = () => {
     
-    sessionStorage.removeItem('studyPlanData');
+    //sessionStorage.removeItem('studyPlanData');
     this.filter.setSaveSuccess(false);
     if (this.data.length === 0) {
       this.filter.getProgramsData();
