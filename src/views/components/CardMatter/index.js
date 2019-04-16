bb23c06a7609bd6c8ea636db5add6dd70b624278
Juan Fernando Restrepo <JuanRestrepo>
2019-04-16 11:02:40 -0500

    Se configuran homologaciones de Componente de Formacion

--- src/views/components/CardMatter/index.js
+++ src/views/components/CardMatter/index.js
@@ -7,22 +7,24 @@ const { Meta } = Card;
 
 class CardMatter extends Component {
 
-    getStyle = (tipoFormacion) => {
+    getAdditionalInformation = (tipoFormacion) => {
       return new Map([
-        ['CBI', '#78909c'],
-        ['FC', '#026F35'],
-        ['IA', '#2196f3'],
-        ['CB', '#f9a825'], 
-        ['O', '#b71c1c']
+        ['Ciencia basica de Ingenieria', { color: '#78909c', nomenclatura: 'CBI' }],
+        ['Formacion complementaria', { color: '#026F35', nomenclatura: 'FC' }],
+        ['Ingenieria aplicada', { color: '#2196f3', nomenclatura: 'IA' }],
+        ['Ciencia basica', { color: '#f9a825', nomenclatura: 'CB' }], 
+        ['Optativa interdisciplinaria', { color: '#b71c1c', nomenclatura: 'O' }]
       ]).get(tipoFormacion);
     }
+
     
     render = () => {
+        const additionalInformation = this.getAdditionalInformation(this.props.id);
         return(
             <Card
-                bodyStyle={{ background: this.getStyle('CBI') }}
+                bodyStyle={{ background: additionalInformation.color }}
                 actions={[
-                    <li>{this.props.formacionComplementaria}</li>,
+                    <li>{additionalInformation.nomenclatura}</li>,
                     <li>{this.props.creditos}</li>,
                     <li>{this.props.horasTeoricas}</li>,
                     <li>{this.props.horasLaboratorio}</li>
