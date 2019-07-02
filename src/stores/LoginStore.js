343d6e28062dce82a9be17f6e5b3ac017ee00c23
Juan Fernando Restrepo <JuanRestrepo>
2019-07-02 07:13:12 -0500

    Se agrega mensaje cuando no se puede obtener respuesta del servidor en el login

--- src/stores/LoginStore.js
+++ src/stores/LoginStore.js
@@ -2,45 +2,46 @@ import { observable } from "mobx";
 
 export default class LoginStore {
 
-        @observable loginService;
-        @observable addUserResponseData;
-        @observable validateLoginUserResponse;
-        @observable addNewUserSuccess = false;
-        @observable isAuthenticated = false;
-        @observable loginOutDisabled = false;
+    @observable loginService;
+    @observable addUserResponseData;
+    @observable validateLoginUserResponse;
+    @observable addNewUserSuccess = false;
+    @observable isAuthenticated = false;
+    @observable loginOutDisabled = false;
 
-        constructor(loginService, process) {
-            this.loginService = loginService;
-            this.process = process;
-        }
+    constructor(loginService, process) {
+        this.loginService = loginService;
+        this.process = process;
+    }
 
-        validateLoginUser = async (loginUserDTO) => {
-          
-            this.process.processDTO.loading = true;
-            this.process.processDTO.loadingMessage = 'VALIDANDO USUARIO ...';
-            await this.loginService.loginUser(loginUserDTO)
+    validateLoginUser = async (loginUserDTO) => {
+
+        this.process.processDTO.loading = true;
+        this.process.processDTO.loadingMessage = 'VALIDANDO USUARIO ...';
+        await this.loginService.loginUser(loginUserDTO)
             .then(response => {
 
                 const { data } = response;
-                this.validateLoginUserResponse = { "data": data , "accesstoken": response.headers.accesstoken };
+                this.validateLoginUserResponse = { "data": data, "accesstoken": response.headers.accesstoken };
                 this.validateUserSuccess = true;
                 this.process.processDTO.loading = false;
-                
+
             })
             .catch(error => {
-                const message = error.response ? `${error.response.data.codigo}: ${error.response.data.mensaje}` : 'ERROR';
+
+                const message = error.response ? `${error.response.data.codigo}: ${error.response.data.mensaje}` : 'No se pudo obtener respuesta del servidor, intente nuevamente';
                 this.process.showMessage(message, 'error');
                 this.process.processDTO.loading = false;
             });
 
-        };
+    };
 
-        setIsAuthenticated = (isAuthenticated) => {
-            this.isAuthenticated = isAuthenticated;
-        };
+    setIsAuthenticated = (isAuthenticated) => {
+        this.isAuthenticated = isAuthenticated;
+    };
 
-        setLoginOutDisabled = (loginOutDisabled) => {
-            this.loginOutDisabled = loginOutDisabled;
-        };
+    setLoginOutDisabled = (loginOutDisabled) => {
+        this.loginOutDisabled = loginOutDisabled;
+    };
 
 }
